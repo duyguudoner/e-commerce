d2071adc7e14b702d28cecd5899186dc6f000381
SevenInterval <ridvanozturk94@gmail.com>
2020-06-07 01:00:09 +0300

    Main dosyası içerisine commit denemesi yapıldı

--- src/main/java/com/ticaret/eticaret/EticaretApplication.java
+++ src/main/java/com/ticaret/eticaret/EticaretApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EticaretApplication {
 
     public static void main(String[] args) {
+        System.out.println("İlk deneme commit'i");
         SpringApplication.run(EticaretApplication.class, args);
     }
 
