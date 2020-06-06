6ba70c144d8136f101401a890cd2848633efa4b7
SevenInterval <ridvanozturk94@gmail.com>
2020-06-07 01:06:09 +0300

    Main system out println'ler silindi.

--- src/main/java/com/ticaret/eticaret/EticaretApplication.java
+++ src/main/java/com/ticaret/eticaret/EticaretApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EticaretApplication {
 
     public static void main(String[] args) {
-        System.out.println("İlk deneme commit'i");
-        System.out.println("İkinci deneme commit'i");
         SpringApplication.run(EticaretApplication.class, args);
     }
 
