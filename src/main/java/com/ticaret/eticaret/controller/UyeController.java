df40a4ed81ab56e94bbb73df7988642a1aca8a52
duyguudoner <duyguudoner@gmail.com>
2020-06-12 00:08:23 +0300

    UyeController sınıfı güncellendi

--- src/main/java/com/ticaret/eticaret/controller/UyeController.java
+++ src/main/java/com/ticaret/eticaret/controller/UyeController.java
@@ -1,10 +1,9 @@
 package com.ticaret.eticaret.controller;
 
 import com.ticaret.eticaret.model.Uye;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ticaret.eticaret.repository.UyeRepository;
 
 import java.util.List;
@@ -20,5 +19,27 @@ public class UyeController {
         return uyeRepository.findAll();
     }
 
+    @GetMapping
+    @RequestMapping("{id}")
+    public Uye get(@PathVariable Long id){
+        return uyeRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Uye create(@RequestBody final Uye uye){
+        return uyeRepository.saveAndFlush(uye);
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+         uyeRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.PUT)
+    public Uye update(@PathVariable Long id,@RequestBody Uye uye) {
+        Uye existingUye = uyeRepository.getOne(id);
+        BeanUtils.copyProperties(uye,existingUye,"uyeno");
+        return uyeRepository.saveAndFlush(uye);
+    }
 
 }
